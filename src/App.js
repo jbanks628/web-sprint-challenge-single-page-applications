204ee735bb6388eba496ce1b1c3ab71704975726
Jada Banks <jadabanks@Jadas-MacBook-Air.local>
2021-12-17 13:40:40 -0600

    trying to get form to show

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Form from './Form';
+
+
 
 const App = () => {
+
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+    <div className='pizzaPage'>
+      
+        <h1>Bloomer Eats</h1>
+        <h2>Eat like a real Bloomeranian!</h2>
+
+        <Link to='/'>
+        <button id='order-pizza'>
+          Home
+        </button>
+      </Link>
+
+      <Link to='/pizza'>
+        <button id='pizza-form'>
+          Build-A-Pizza
+        </button>
+      </Link>
+
+      <Link to='/complete'>
+        Complete Order
+      </Link>
+
+      <Switch>
+        <Route path='/'/>
+        <Route path='/pizza' component={Form}>
+        </Route>
+      </Switch>
+
+    </div>
+    </Router>
   );
 };
 export default App;
